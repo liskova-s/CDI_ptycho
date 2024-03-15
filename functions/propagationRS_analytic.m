30de27097ac7a278bea4f3d8259ecec81a5cf5ed
liskova-s <79690997+liskova-s@users.noreply.github.com>
2024-03-15 13:44:48 +0100

    Update propagationRS_analytic.m sampling resolved

--- functions/propagationRS_analytic.m
+++ functions/propagationRS_analytic.m
@@ -1,17 +1,19 @@
 function [FX,FY,u2] = propagationRS_analytic(U, lambda, z, squaresize)
+    [M,~] = size(U); 
+    dx1 = squaresize;
+    L1 = M*dx1; 
+   
+    L2 = lambda*z/dx1; 
+    dx2 = lambda*z/L1;
+    
+    x2 = -L2/2:dx2:L2/2-dx2; 
+    [FX, FY] = meshgrid(x2, x2);
     k = 2*pi/lambda;
-    dx = squaresize;
-    [M, N] = size(U); 
-    
-    % Spatial frequencies
-    fx = (-N/2:N/2-1) / (N*dx);
-    fy = (-M/2:M/2-1) / (M*dx);
-    [FX, FY] = meshgrid(fx, fy);
-    
     % Quadratic phase factor 
     H = (exp(-1i * k * z * sqrt(1 - lambda^2 * (FX.^2 + FY.^2))));
     H(isnan(H)) = 0; 
     
     u2 = fftshift(fft2((U .* H)));
     u2 = u2./max(max(u2)).*max(max(U));
+    u2 = u2 / max(max(abs(u2)));
 end
