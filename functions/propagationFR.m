249aa09e94eb595cf274381d7775337396e0a646
liskova-s <79690997+liskova-s@users.noreply.github.com>
2024-04-02 09:11:51 +0200

    Incorrect rsq formula fixed

--- functions/propagationFR.m
+++ functions/propagationFR.m
@@ -1,6 +1,6 @@
 function u2 = propagationFR(wave, coordinates, lambda, z)
     k = 2*pi/lambda;
-    rsq = (coordinates(:,:,1).^2+coordinates(:,:,2).^2+z.^2);
+    rsq = (coordinates(:,:,1).^2+coordinates(:,:,2).^2);
     h = exp(1i*k*z)/(1i*lambda*z)*exp(1i*k/(2*z)*rsq);
      % padding
     mm = size(wave,1) + size(h,1) - 1;
